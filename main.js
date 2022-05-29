5807693af254f182201b7526440ea10017d4285d
IC20201006\tanaka <tanaka_yoshinao@emplayee.co.jp>
2022-05-30 01:54:44 +0900

    一旦コミット

--- main.js
+++ main.js
@@ -3,34 +3,42 @@ $(function () {
     var name = "カードをかざしてください"
     var account = ""
     var password = ""
+    //カード番号を取得してるかどうか
     var isCardIdGetted = false
     var getCardIdTimer
 
-    
     document.getElementById('name').textContent = name
-
 
     startGetCardIdTimer()
 
-
-
-
     $('#attendance_button').click(function() {
         alert(name + "さんが出勤しました")
-        //ここにDBのかーどIDを消す処理
         initialize()
         reloadDisplay()
+        isCardIdGetted = false
     });
     $('#leaving_button').click(function() {
         alert(name + "さんが退勤しました")
-        //ここにDBのかーどIDを消す処理
         initialize()
         reloadDisplay()
+        isCardIdGetted = false
+    });
+
+    $('#cacncel_button').click(function() {
+        alert("キャンセルされました")
+        initialize()
+        reloadDisplay()
+        isCardIdGetted = false
     });
 
 
     function getCardId() {
-        console.log("test")
+        //カード情報を保持してない時のみDBにカード情報を取りに行く
+        if (!isCardIdGetted){
+            //ここでDBのカード番号を取得する処理
+            //取得出来たらname等に情報を格納、isCardIdGettedをtrueにする。DBのカード番号を削除する
+            console.log("test")
+        }
     }
 
     function initialize() {
