05bdbced70cfa8ea8a819acd8578cf28788e5500
IC20201006\tanaka <tanaka_yoshinao@emplayee.co.jp>
2022-05-29 23:54:16 +0900

    一旦コミット

--- main.js
+++ main.js
@@ -3,20 +3,50 @@ $(function () {
     var name = "カードをかざしてください"
     var account = ""
     var password = ""
+    var isCardIdGetted = false
+    var getCardIdTimer
+
     
     document.getElementById('name').textContent = name
 
-    var test = () => {
-        console.log("test")
-    }
 
-    setInterval(test,1000)
+    startGetCardIdTimer()
 
 
 
 
     $('#attendance_button').click(function() {
-        document.getElementById('name').textContent = "田中"
+        alert(name + "さんが出勤しました")
+        //ここにDBのかーどIDを消す処理
+        initialize()
+        reloadDisplay()
     });
+    $('#leaving_button').click(function() {
+        alert(name + "さんが退勤しました")
+        //ここにDBのかーどIDを消す処理
+        initialize()
+        reloadDisplay()
+    });
+
+
+    function getCardId() {
+        console.log("test")
+    }
+
+    function initialize() {
+        name = "カードをかざしてください"
+        account = ""
+        password = ""
+    }
+
+    function reloadDisplay() {
+        document.getElementById('name').textContent = name
+        document.getElementById('account').value = account
+        document.getElementById('password').value = password
+    }
+
+    function startGetCardIdTimer() {
+        getCardIdTimer = setInterval(getCardId,1000)
+    }
   
   });
