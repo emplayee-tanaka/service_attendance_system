a46f0c4632872f71f21d97ee23b4cff4b6ae98bf
IC20201006\tanaka <tanaka_yoshinao@emplayee.co.jp>
2022-05-29 16:13:48 +0900

    一旦コミット

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 $(function () {
 
-    $('.calbutton').click(function() {
-  
+    var name = "カードをかざしてください"
+    var account = ""
+    var password = ""
+    
+    document.getElementById('name').textContent = name
 
+    var test = () => {
         console.log("test")
+    }
+
+    setInterval(test,1000)
+
+
+
+
+    $('#attendance_button').click(function() {
+        document.getElementById('name').textContent = "田中"
     });
   
   });
