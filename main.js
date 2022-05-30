6dd29089e27235696ac8a628ebe629e30ae0394d
IC20201006\tanaka <tanaka_yoshinao@emplayee.co.jp>
2022-05-31 05:45:28 +0900

    一旦コミット

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ $(function () {
     var password = ""
     //カード番号を取得してるかどうか
     var isCardIdGetted = false
+    var gettedCardNumberArray = []
     var getCardIdTimer
 
     document.getElementById('name').textContent = name
@@ -16,6 +17,7 @@ $(function () {
             alert(name + "さんが出勤しました")
             initialize()
             reloadDisplay()
+            deleteCardNumberFromDB(gettedCardNumberArray[0])
         } else {
             alert("カードをかざしてください")
         }
@@ -25,26 +27,22 @@ $(function () {
             alert(name + "さんが退勤しました")
             initialize()
             reloadDisplay()
+            deleteCardNumberFromDB(gettedCardNumberArray[0])
         } else {
             alert("カードをかざしてください")
         }
     });
 
     $('#cacncel_button').click(function() {
+        if (isCardIdGetted){
+            deleteCardNumberFromDB(gettedCardNumberArray[0])
+        }
         alert("キャンセルされました")
         initialize()
         reloadDisplay()
     });
 
-    $('#login_button').click( async function() {
-
-        fetch('https://bj1ci45en5.execute-api.us-west-2.amazonaws.com/test', {
-        }).then(response => {
-            console.log(response);
-          });
-
-
-
+    $('#login_button').click(function() {
         isCardIdGetted = true
         name = "テスト太郎"
         account = "testtarou"
@@ -53,11 +51,42 @@ $(function () {
     });
 
 
-    function getCardId() {
+    async function getCardId() {
         //カード情報を保持してない時のみDBにカード情報を取りに行く
         if (!isCardIdGetted){
-            //ここでDBのカード番号を取得する処理
-            //取得出来たらname等に情報を格納、isCardIdGettedをtrueにする、reloadDisplay()、DBのカード番号を削除する
+          try {
+            const response = await fetch('https://bj1ci45en5.execute-api.us-west-2.amazonaws.com/test/getcardnumber', {
+              method: "GET",
+              mode: 'cors'
+            });
+        
+            if (response.ok) {
+              const resJson = await response.json();
+              const responseCardNumbers = JSON.parse(resJson.body).Items
+              responseCardNumbers.forEach(element => gettedCardNumberArray.push(element.cardNumber));
+              if (gettedCardNumberArray[0]){
+                //isCardIdGettedをtrueにしてDBアクセスを止める
+                isCardIdGetted = true
+                if (gettedCardNumberArray[0] == 123){
+                    name = "田中　一郎"
+                    account = "tanaka_ichiro"
+                    password = "tanaka1rou"
+                    reloadDisplay()
+                } else if (gettedCardNumberArray[0] == 456){
+                    name = "山田　二郎"
+                    account = "yamada_jiro"
+                    password = "yamada2rou"
+                    reloadDisplay()
+                }
+              }
+
+            } else {
+              throw new Error('Network response was not ok.');
+            }
+        
+          } catch (error) {
+            console.error(error);
+          }
             console.log("test")
         }
     }
@@ -78,5 +107,40 @@ $(function () {
     function startGetCardIdTimer() {
         getCardIdTimer = setInterval(getCardId,2000)
     }
-  
-  });
+
+    async function deleteCardNumberFromDB(cardNumber) {
+        try {
+          const response = await fetch('https://bj1ci45en5.execute-api.us-west-2.amazonaws.com/test/deletecardnumber', {
+            method: "PUT",
+            mode: 'cors',
+            cardNumber: cardNumber
+          });
+          if (response.ok) {
+            ///ログ出力
+          } else {
+            throw new Error('Network response was not ok.');
+          }
+      
+        } catch (error) {
+          console.error(error);
+        }
+    }
+    
+    // async function putCardNumberFromDB(cardNumber) {
+    //     try {
+    //       const response = await fetch('https://bj1ci45en5.execute-api.us-west-2.amazonaws.com/test/postcardnumber', {
+    //         method: "PUT",
+    //         mode: 'cors',
+    //         cardNumber: cardNumber
+    //       });
+      
+    //       if (response.ok) {
+    //         ///ログ出力
+    //       } else {
+    //         throw new Error('Network response was not ok.');
+    //       }      
+    //     } catch (error) {
+    //       console.error(error);
+    //     }
+    // }
+});
